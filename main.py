12ceb9cfbd400c5cf657e48488140931f4d59d09
川扬 <chuanyang.yb@alibaba-inc.com>
2022-01-19 17:45:03 +0800

    sdcdscsdcsdcsdcsdcsdcsdcsd

--- main.py
+++ main.py
@@ -8,8 +8,9 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
-
-
+    print("this is the main part")
+    print("fsafsfsfsf")
+    print("sfsdfsdf")
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
