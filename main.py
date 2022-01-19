592b4ea2dd2f557a2e1a8665a6f7812244f54974
川扬 <chuanyang.yb@alibaba-inc.com>
2022-01-19 17:45:42 +0800

    cd]

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
     print("this is the main part")
     print("fsafsfsfsf")
     print("sfsdfsdf")
+    print("csfsdf")
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
